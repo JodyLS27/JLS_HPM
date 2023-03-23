9a0955623c8b74dd806c8cebd3a9fbefb6215d29
Jody <jodystocks01@gmail.com>
2023-03-23 09:01:11 +0200

    Updated main_window.py - Added Menu bar intergration from menu_bar.py

--- src/main_window.py
+++ src/main_window.py
@@ -1,5 +1,7 @@
 from PySide6.QtWidgets import QMainWindow
 
+from GUI import menu_bar
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,4 +10,9 @@ class MainWindow(QMainWindow):
 
         self.setMinimumWidth(350)
         self.setMinimumHeight(400)
+        
+        
+        self.menuBar = menu_bar.MenuBar()
+        self.setMenuBar(self.menuBar)
+
         
