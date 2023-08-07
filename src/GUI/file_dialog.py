c43115a443eb6ad7141c414b47217a8284c8b640
Jody Stocks <jodystocks01@gmail.com>
2023-08-07 13:37:55 +0200

    Code cleanup with PEP8 Standard

--- src/GUI/file_dialog.py
+++ src/GUI/file_dialog.py
@@ -12,7 +12,7 @@ class FileDialogPopup(QFileDialog):
         super().__init__()
 
         dialog = QFileDialog(self)
-        file_path = Path(dialog.getExistingDirectory(self, "Please select a desired location", str(Path.home())))
+        file_path = Path(dialog.gecleatExistingDirectory(self, "Please select a desired location", str(Path.home())))
 
         text = "This is a test. \nWondering what will be stored and what will break."
         rw.write_text(file_path, text, "test")
